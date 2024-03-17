a12d675b0c31a11b711428a954a4df2f7a69e0f9
Emanuele Frasca <emanuele.frasca.00@gmail.com>
2024-03-17 11:29:13 +0100

    add install_requirements.sh bash script

---
+++ install_requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Run this script to install prerequisites in Ubuntu 20.04
+
+# Update the repository list
+sudo apt update -y
+
+# Use single-line installer to install ROS-noetic
+wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh
+
+# Install OpenCV library
+sudo apt install -y libopencv-dev
+
+# Install yaml-cpp library
+sudo apt install -y libyaml-cpp-dev
